60d9146fe10c48a9353a86f2284dee778eebb63c
배지영 <120749541+qowldud@users.noreply.github.com>
2026-04-04 16:31:35 +0900

    [CHORE]: 브랜드 컬러 토큰 추가 및 초기 템플릿 CSS 제거 (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Button>버튼</Button>
-      <div className="w-10 h-10 bg-red-500">안녕</div>
+      <div className="w-10 h-10 bg-mate-100">안녕</div>
     </>
   );
 }
