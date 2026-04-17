dca8fd32ecbfb6a87bc63aeeadf6bf612565b863
qowldud <bjy6631@daum.net>
2026-04-17 16:41:02 +0900

    chore: Tanstack Router 설정 및 홈라우트 세팅

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-// 홈 페이지 라우트 (/)
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: HomePage,
+})
+
+function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1>홈</h1>
+    </div>
+  )
+}
