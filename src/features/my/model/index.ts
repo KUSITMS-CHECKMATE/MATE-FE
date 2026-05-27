ae54aeff95f026e68e31ac0fed15745ab929faa7
홍석주 <hsj202201@gmail.com>
2026-05-27 20:55:14 +0900

    feat: 마이 참여 기록 api 연결

--- src/features/my/model/index.ts
+++ src/features/my/model/index.ts
@@ -1,2 +1,3 @@
 export type { MyUser, Notice, ParticipateRecord, TestDetail } from './types';
 export { mockMyUser, mockNotices, mockParticipateRecords, mockTestDetails } from './mock';
+export { useMyParticipateHistory } from './api';
